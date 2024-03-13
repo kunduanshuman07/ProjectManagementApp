d72e711868492f04d7ca08ec22f1c612b70b32b6
Anshuman Kundu <kundu4coding@gmail.com>
2024-03-13 13:11:02 +0530

    Final 4

--- pm-app/app/page.tsx
+++ pm-app/app/page.tsx
@@ -8,32 +8,10 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  const [user, setUser] = useState<any>(null);
-  useEffect(() => {
-    // Check if window is defined (running in the browser)
-    if (typeof window !== 'undefined') {
-      const storedUser = localStorage?.getItem('user');
-      if (storedUser === null) {
-        router.push('/login');
-      } else {
-        setUser('logged in');
-      }
-    }
-  }, [router]);
   return (
     <main>
-      {user === null ?
-        <div className="flex flex-row">
-          <div className='m-auto mt-20'>
-            <span className="loading loading-bars text-neutral loading-lg ml-2"></span>
-            <span className="loading loading-bars text-neutral loading-lg ml-2"></span>
-            <span className="loading loading-bars text-neutral loading-lg ml-2"></span>
-          </div>
-        </div> :
-        <>
-          <Drawer />
-          <Dashboard />
-        </>}
+      <Drawer />
+      <Dashboard />
     </main>
   );
 }
