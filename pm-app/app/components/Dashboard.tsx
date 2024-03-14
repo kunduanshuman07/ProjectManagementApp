937c79a91bb15f85ca5f5de7646f3e100b2e72b1
Anshuman Kundu <kundu4coding@gmail.com>
2024-03-14 21:51:47 +0530

    dashboard tiles responsive check

--- pm-app/app/components/Dashboard.tsx
+++ pm-app/app/components/Dashboard.tsx
@@ -6,11 +6,12 @@ import { FaEye } from 'react-icons/fa';
 import { useRouter } from "next/navigation";
 import { fetchDashboardData } from '../server-actions/fetchDashboardData';
 
-interface DashboardProps{
+interface DashboardProps {
     loginverification: boolean;
 }
 
-const Dashboard:React.FC<DashboardProps> = ({loginverification}) => {
+const Dashboard: React.FC<DashboardProps> = ({ loginverification }) => {
+    const screenWidth = window.screen.availWidth;
     const [loading, setLoading] = useState<boolean>(true);
     const [dashboardData, setDashboardData] = useState<any>();
     useEffect(() => {
@@ -23,15 +24,15 @@ const Dashboard:React.FC<DashboardProps> = ({loginverification}) => {
         }
         fetchDashboard();
     }, []);
-    useEffect(()=>{
+    useEffect(() => {
 
-    },[])
+    }, [])
     const [modalOpen, setModalOpen] = useState<boolean>(false);
     const router = useRouter();
     const handleViewTasks = () => {
         router.push('/projects');
     }
-    
+
     return (
         <div className='flex flex-col'>
             {loading ?
@@ -45,20 +46,20 @@ const Dashboard:React.FC<DashboardProps> = ({loginverification}) => {
                 <>
                     <div className='pb-10 bg-neutral'>
                         <h1 className='mt-5 ml-10 font-bold bg-accent text-white w-60 p-3 rounded text-center'>Dashboard Analytics</h1>
-                        <div className='flex flex -row mt-5 ml-10'>
+                        <div className={`flex mt-5 ${screenWidth < 1000 ? 'flex-col mx-auto ml-10 mr-10' : 'flex-row ml-10'}`}>
                             <div className="stats shadow">
                                 <div className="stat place-items-center">
                                     <div className="stat-title">Projects</div>
                                     <div className="stat-value mt-3">{loading ? <span className="loading loading-dots loading-md"></span> : dashboardData.ProjectsCount} </div>
                                 </div>
                             </div>
-                            <div className="stats shadow ml-5">
+                            <div className={`stats shadow ${screenWidth < 1000 ? 'mt-3' : 'ml-5'}`}>
                                 <div className="stat place-items-center">
                                     <div className="stat-title font-bold">Users</div>
                                     <div className="mt-3 stat-value text-info">{loading ? <span className="loading loading-dots loading-md"></span> : dashboardData.UsersCount} </div>
                                 </div>
                             </div>
-                            <div className="stats shadow ml-5">
+                            <div className={`stats shadow ${screenWidth < 1000 ? 'mt-3' : 'ml-5'}`}>
                                 <div className="stat place-items-center">
                                     <div className="stat-title font-bold">Tasks</div>
                                     <div className="stat-value mt-3">{loading ? <span className="loading loading-dots loading-md"></span> : dashboardData.TasksCount} </div>
@@ -68,17 +69,17 @@ const Dashboard:React.FC<DashboardProps> = ({loginverification}) => {
                                     <div className="stat-value mt-3 text-primary">{loading ? <span className="loading loading-dots loading-md"></span> : dashboardData.IpCount} </div>
                                 </div>
                             </div>
-                            <div className="stats shadow ml-5">
+                            <div className={`stats shadow ${screenWidth < 1000 ? 'mt-3' : 'ml-5'}`}>
                                 <div className="stat place-items-center">
                                     <div className="stat-title font-bold">High Priority</div>
                                     <div className="stat-value mt-3 text-error">{loading ? <span className="loading loading-dots loading-md"></span> : dashboardData.HighCount} </div>
                                 </div>
                             </div>
-                            <div className="stats shadow ml-5">
+                            <div className={`stats shadow ${screenWidth < 1000 ? 'mt-3' : 'ml-5'}`}>
                                 <div className="stat place-items-center">
                                     <div className="stat-title font-bold">Closest Project</div>
                                     <div className="mt-3 font-bold text-info">{loading ? <span className="loading loading-dots loading-md"></span> : dashboardData.ClosestProject?.project_title} </div>
-                                    <div className="stat-desc mt-3 text-neutral font-bold">{loading ? <span className="loading loading-dots loading-md"></span> : `Deadline: ${dashboardData.ClosestProject?dashboardData.ClosestProject.deadline: ''}`}</div>
+                                    <div className="stat-desc mt-3 text-neutral font-bold">{loading ? <span className="loading loading-dots loading-md"></span> : `Deadline: ${dashboardData.ClosestProject ? dashboardData.ClosestProject.deadline : ''}`}</div>
                                 </div>
                             </div>
                         </div>
@@ -88,7 +89,7 @@ const Dashboard:React.FC<DashboardProps> = ({loginverification}) => {
                         <button className='btn mt-5 ml-10 btn-neutral' onClick={handleViewTasks}>View All Tasks <FaEye /></button>
                     </div>
                     <ProjectTable />
-                    {modalOpen && <AddProjectModal modalOpen={modalOpen} setModalOpen={setModalOpen} loginverification={loginverification}/>}
+                    {modalOpen && <AddProjectModal modalOpen={modalOpen} setModalOpen={setModalOpen} loginverification={loginverification} />}
                 </>}
         </div>
     )
